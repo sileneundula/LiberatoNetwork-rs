d141e66ce7cde29eafbf1c8ace66d670807b90cd
silene0259 <158784347+sileneundula@users.noreply.github.com>
2025-12-11 09:36:43 -0500

    Update mod.rs

--- shrine-network/src/app/mod.rs
+++ shrine-network/src/app/mod.rs
@@ -1,3 +1,4 @@
+use libp2p::core::transport::ListenerId;
 use libp2p::futures::StreamExt;
 use log::{warn,info, trace, debug};
 use log::error;
@@ -9,7 +10,13 @@ use libp2p::floodsub::Topic;
 
 use libp2p::swarm::SwarmEvent;
 
+use libp2p::Multiaddr;
+
+use std::str::FromStr;
+
 use crate::core::internals::Utils;
+
+use libp2p::swarm::ListenAddresses;
 
 use dotenvy;
 pub mod bootstrap;
@@ -18,6 +25,9 @@ const swarm_creation: &str = "0x22";
 const SHRINDO_TOPIC: &str = "shrindo-0x20CB-v1";
 const SHRINDO_TOPIC_BOOTSTRAP: &str = "shrindo-0x20CB-bootstrap-v1";
 const SHRINDO_TOPIC_KEYEXCHANGE: &str = "shrindo-0x20CB-keyexchange";
+const SHRINDO_TOPIC_ESSENTIALS: &str = "Lament-Essentials";
+
+const local_address: &str = "/ip4/127.0.0.1/tcp/9076";
 
 pub struct ShrindoApp;
 
@@ -29,6 +39,7 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     pretty_env_logger::init();
     debug!("Pretty Env Logger Initialized...");
 
+    info!("Generating Ephermal ED25519 Key...");
     // Key Generation
     let key = crate::networking::internals::keys::ShrineKeys::generate_ed25519();
     let local_peer_id = key.key.public().to_peer_id();
@@ -38,25 +49,34 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut swarm: libp2p::Swarm<crate::networking::internals::behavior::ShrineBehaviour> = ShrineSwarm::new(key);
     info!("Swarm Creation ID: {}", Utils::create_swarm_id(local_peer_id).unwrap());
 
+    //=====BOOTSTRAP CLI=====//
+
+
     // Store + Kademelia
     //let store = MemoryStore::new(local_peer_id);
     
     
     // Behaviour
     swarm.behaviour_mut().floodsub.subscribe(Topic::new(SHRINDO_TOPIC));
+    info!("[Floodsub] Subscribed to {}", SHRINDO_TOPIC);
     swarm.behaviour_mut().floodsub.subscribe(Topic::new(SHRINDO_TOPIC_BOOTSTRAP));
+    info!("[Floodsub] Subscribed to {}", SHRINDO_TOPIC_BOOTSTRAP);
 
-    /*
+    let id = swarm.listen_on(Multiaddr::from_str(local_address).unwrap())?;
+
+    info!("[SHRINDO] Connecting to SHRINDO/SHRINE with Peer-ID: {}",local_peer_id);
+
     loop {
+        /// # Event
         let event = swarm.select_next_some().await;
 
         match event {
-            SwarmEvent::Behaviour(libp2p::kad::Event::OutboundQueryProgressed { id, result, stats, step } {
-
-            })
+            SwarmEvent::NewListenAddr { listener_id, address} => {
+                info!("[Swarm] Listening On {} with listener_id: {}", local_address, listener_id);
+            },
+            _ => ()
         }
     }
-    */
 
     Ok(())
 
