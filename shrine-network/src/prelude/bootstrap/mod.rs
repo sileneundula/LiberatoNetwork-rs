302a3a0122b8bbfab68e3d7c29e53009d4fb8311
silene0259 <158784347+sileneundula@users.noreply.github.com>
2026-01-03 10:26:46 -0500

    Update mod.rs

--- shrine-network/src/prelude/bootstrap/mod.rs
+++ shrine-network/src/prelude/bootstrap/mod.rs
@@ -1,3 +1,7 @@
+//! # \[Deprecated] Bootstrapping Prelude
+//! 
+//! Contains all needed data for Bootstrapping
+
 // Bootstrapping
 pub use crate::app::bootstrap::BootstrapAddressesModular;
 pub use crate::app::bootstrap::BootstrapProtocol;
