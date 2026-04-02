7da2da7c883a7be1a9fb216863c9a61381113611
silene0259 <158784347+sileneundula@users.noreply.github.com>
2026-04-02 09:48:50 -0400

    Update cli.rs

--- shrine-network/src/cli/core/cli.rs
+++ shrine-network/src/cli/core/cli.rs
@@ -1,21 +1,133 @@
+//! # MuscarineNetwork-CLI
+//! 
+//! This module contains all the commands for MuscarineNetwork.
+//! 
+//! ## Description
+//! 
+//! MuscarineNetwork offers Command-Line-Interface usage for users that is both extensible and easy to use. It aims to create a decentralized network where Peer-2-Peer communication can be achieved.
+
+
 use clap::Parser;
 
+/// # Custom Protocol ID
+/// 
+/// A fixedstr of 256 bytes for calling custom protocols.
+type CUSTOM_PROTOCOL_ID = fixedstr::str256;
+
+#[derive(Debug,Clone,PartialEq,PartialOrd,Hash)]
 pub struct MuscarineCommands {
     action: String,
 
     config: String,
 }
 
-pub enum MuscarineActionCommands {
-    ls,
-    retv, // retrieve
+#[derive(Debug,Clone,Copy,PartialEq,PartialOrd,Hash)]
+pub struct MuscarineCommandHandler {
+    action: MuscarineActionCommands,
 }
 
-pub enum MuscarineListCommands {
-    peers,
+/// # MuscarineActionCommands
+/// 
+/// ## Commands
+/// 
+/// - [ ] `ls`: List
+/// - [ ] `retv`: Retrieve
+/// - [ ] `whoami`: info
+/// - [ ] `version`: Version info
+/// - [ ] `protocols`: Lists protocols
+///     - [ ] 
+/// 
+/// ## Extension Commands
+/// 
+/// - [ ] `muscarine`
+///     - [ ] `muscarine-keys`
+/// 
+///     - [ ] `muscarine app <>`
+///     - [ ] `muscarine ext <>`
+///     - [ ] `muscarine ol-app <>`
+///     - [ ] `muscarine`
+/// 
+/// ## Implemented
+/// 
+/// - [ ] 
+#[derive(Debug,Clone,Copy,PartialEq,PartialOrd,Hash)]
+pub enum MuscarineActionCommands {
+    ls(MuscarineListCommands),
+    retv(MuscarineRetrieveCommands), // retrieve
+    whoami(MuscarineWhoAmICommands), // whoami
+    version,
+    protocols,
+    audit(MuscarineAuditCommands), // Audits information
     
-    files,
-    trustnet,
-    bootstrap,
-    domains,
+    // Muscarine Subcommands
+    muscarine(MuscarineSubcommands),
+
+    muscarine_protocols(MuscarineExtensibleProtocol), // muscarine-protocols
+
+    // /ol-apps/
+    ol_apps(MuscarineOlApps)
+}
+
+#[derive(Debug,Clone,Copy,PartialEq,PartialOrd,Hash)]
+pub enum MuscarineSubcommands {
+    MuscarineKeys(), // muscarine-keys
+    MuscarineApps(), // muscarine-apps
+    MuscarineInterop(), // muscarine-interop
+    MuscarineBootstrap(), // muscarine-bootstrap
+    MuscarineProtocol(), // muscarine-protocol
+    MuscarineServices(), // muscarine-services
+}
+
+/// # MuscarineExtensibleProtocol
+/// 
+/// Includes the Standard Library and Custom Protocol Usage
+#[derive(Debug,Clone,Copy,PartialEq,PartialOrd,Hash)]
+pub enum MuscarineExtensibleProtocol {
+    Standard(MuscarineStandardProtocols),
+    CustomProtocol(CUSTOM_PROTOCOL_ID),
+}
+
+#[derive(Debug,Clone,Copy,PartialEq,PartialOrd,Hash)]
+pub enum MuscarineStandardProtocols {
+    PullFromChain,
+}
+
+#[derive(Debug,Clone,Copy,PartialEq,PartialOrd,Hash)]
+pub enum MuscarineAuditCommands {
+    peers,
+}
+
+#[derive(Debug,Clone,Copy,PartialEq,PartialOrd,Hash)]
+pub enum MuscarineOlApps {
+    Install,
+    Remove,
+}
+
+#[derive(Debug,Clone,Copy,PartialEq,PartialOrd,Hash)]
+pub enum MuscarineListCommands {
+    peers, // p: Peers connected to
+    files, // f: Files in the sandboxed directory
+    trustnet, // tn: TrustNet Node Addresses
+    bootstrap, // b: Bootstrapping Nodes
+    domains, // dns: Domain Names
+    certificates, // cert: Certificates
+    protocols, // -proto: Lists protocols
+    
+    // /ol-apps/
+    extensions, // exts: Local Extensions to the protocol
+    apps, // apps: Local Apps to the protocol
+}
+
+#[derive(Debug,Clone,Copy,PartialEq,PartialOrd,Hash)]
+pub enum MuscarineRetrieveCommands {
+    address, // addr: Retrieves an address
+    multiaddr, // multi: Retrieves a MultiAddr
+    hash, // h: hash retrieve
+}
+
+#[derive(Debug,Clone,Copy,PartialEq,PartialOrd,Hash)]
+pub enum MuscarineWhoAmICommands {
+    info, // i: Information
+    user, // u: User information
+    slab, // slab: Slab of Info
 }
