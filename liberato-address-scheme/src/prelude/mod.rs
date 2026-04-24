56bbf11077ad424acd29b8bcfdeab01b06498b31
silene0259 <158784347+sileneundula@users.noreply.github.com>
2026-04-24 16:39:44 -0400

    Update mod.rs

--- liberato-address-scheme/src/prelude/mod.rs
+++ liberato-address-scheme/src/prelude/mod.rs
@@ -2,6 +2,8 @@
 pub use crate::identity::cipher_suite::CipherSuite;
 //=====Liberato Identity=====//
 pub use crate::identity::{LiberatoIdentity,LiberatoIdentityAPI,LiberatoIdentityPublic};
+//=====Liberato Signature=====//
+pub use crate::identity::signature::{LiberatoSignature,LiberatoSignatureWithMessage};
 
 pub mod traits {
     //=====Liberato Identity Traits=====//
